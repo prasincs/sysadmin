7c511475a251603552a6d3e633b35c48f177c66a
Prasanna Gautam <prasannagautam@gmail.com>
2011-01-19 15:34:34 -0500

    added script to generate android udev rules

---
+++ android-udev/gen_rules.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import os,stat
+with open ("android_vendors_list") as f:
+    g = open("51-android.rules","w")
+    for line in f.readlines():
+        name, id = line.strip().split(",")
+        g.write("#%s\n"%name)
+        g.write('SUBSYSTEM=="usb_device", SYSFS{idVendor}=="%s", MODE="0666"\n'%id)
+    os.chmod("51-android.rules",stat.S_IXOTH|stat.S_IROTH|stat.S_IRWXU|stat.S_IRWXG)
+    g.close()
